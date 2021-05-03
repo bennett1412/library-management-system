6a28b103d382a44b7f3f4bdbf5540bd0a8a270ec
bennett1412 <bennettmadavana@gmail.com>
2021-05-03 16:02:01 +0530

    Feat: add email feature to forgot password

--- reset-request.php
+++ reset-request.php
@@ -1,62 +1,89 @@
 <?php
-    if(isset($_POST["reset-request-submit"])){
-        $selector = "fc120496849b1e5d"; //bin2hex(random_bytes(8))
-        $token = "x��8�v� Ʃ� Q}Ι�d�.��ֺ�"; //random_bytes(32)
-        //check the url afterwards
-        $url = "http://localhost/lib-man-proj/create-new-pasword.php?selector=" . $selector . "&validator=78efbfbdefbfbd38efbfbd76efbfbd200510c6a910efbfbd1820517dce991b1cefbfbd0164efbfbd2eefbfbdefbfbdd6baefbfbd";
-        $expires = date("U") + 900;
-        //http://localhost/lib-man-proj/login.php
-        require "db_connect.php";
 
-        $userEmail = $_POST["email"];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-        $sql = "DELETE FROM pwdReset WHERE pwdResetEmail = ?;";
-        $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            echo "There was an error!";
-            exit();
-        }
-        else{
-            mysqli_stmt_bind_param($stmt, "s", $userEmail);
-            mysqli_stmt_execute($stmt);
-        }
 
-        $sql ="INSERT INTO pwdReset(pwdResetEmail , pwdResetSelector , pwdResetToken , pwdResetExpires) VALUES (? , ? , ? , ?);";
+require 'phpMailer/vendor/autoload.php';
 
-        $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            echo "There was an error!";
-            exit();
-        }
-        else{
-            $hashedToken = password_hash($token , PASSWORD_DEFAULT);
-            mysqli_stmt_bind_param($stmt, "ssss", $userEmail , $selector , $hashedToken, $expires);
-            mysqli_stmt_execute($stmt);
-        }
+if (isset($_POST["reset-request-submit"])) {
+    $selector = bin2hex(random_bytes(8));
+    $token = random_bytes(32);
+    //check the url afterwards
+    $url = "http://localhost/lib-man-proj/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
+    $expires = date("U") + 900;
+    //http://localhost/lib-man-proj/login.php
+    require "db_connect.php";
 
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
+    $userEmail = $_POST["email"];
+    //VD?3gBTf%5n3xMw(
+    $sql = "DELETE FROM pwdReset WHERE pwdResetEmail = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "There was an error!";
+        exit();
+    } else {
+        mysqli_stmt_bind_param($stmt, "s", $userEmail);
+        mysqli_stmt_execute($stmt);
+    }
 
-        $to = $userEmail;
+    $sql = "INSERT INTO pwdReset(pwdResetEmail , pwdResetSelector , pwdResetToken , pwdResetExpires) VALUES (? , ? , ? , ?);";
 
-        $subject = "Reset your password";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "There was an error!";
+        exit();
+    } else {
+        $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+        mysqli_stmt_bind_param($stmt, "ssss", $userEmail, $selector, $hashedToken, $expires);
+        mysqli_stmt_execute($stmt);
+    }
 
-        $message = '<p>We recieved password reset request. The link to reset your password is given as follows. If you didnt make this request, you can ignore this email</p>';
-        
-        $message .= "<p>Here is your password reset link: <br>";
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 
-        $message .= '<a href = "' . $url . '">' . $url . '</a></p>';
+    // $to = $userEmail;
 
-        $headers = "From LMS <jasleoyo@gmail.com>\r\n";
-        $headers .= "Reply To: jasleoyo@gmail.com\r\n";
-        $headers .= "Content-type: text/html\r\n";
+    // $subject = "Reset your password";
 
-        mail($to , $subject , $message, $headers);
+    $message = '<p>We recieved password reset request. The link to reset your password is given as follows. If you didnt make this request, you can ignore this email</p>';
 
+    $message .= "<p>Here is your password reset link: <br>";
+
+    $message .= '<a href = "' . $url . '">' . $url . '</a></p>';
+
+    // $headers = "From LMS <jasleoyo@gmail.com>\r\n";
+    // $headers .= "Reply To: jasleoyo@gmail.com\r\n";
+    // $headers .= "Content-type: text/html\r\n";
+
+    // mail($to , $subject , $message, $headers);
+
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = '587';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    // $mail->isHTML();
+    $mail->Username = 'libme.library.system@gmail.com';
+    $mail->Password = 'khiladinoek123';
+    $mail->setFrom('no-reply@libMe.com');
+    $mail->Subject = "Reset your password";
+    $mail->Body = $message;
+    $mail->addAddress($userEmail);
+
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        //echo 'Message sent!';
+        //Section 2: IMAP
+        //Uncomment these to save your message in the 'Sent Mail' folder.
+        #if (save_mail($mail)) {
+        #    echo "Message saved!";
+        #}
         header("Location: reset-password.php?reset=success");
     }
-    else{
-        header("Location: ../index.php");
-    }
-
-?>
+} else {
+    header("Location: ../index.php");
+}
